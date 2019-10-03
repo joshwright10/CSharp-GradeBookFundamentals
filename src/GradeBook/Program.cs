0f085d97bdf9cb3527e3c5d0aa875022014e6194
Josh Wright <josh.wright100@gmail.com>
2019-10-03 21:46:41 +0100

    Complete module #7

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -6,17 +6,43 @@ namespace GradeBook
     class Program
     {
 
-
         static void Main(string[] args)
         {
             var book = new Book("Josh's Grade Book");
-            book.AddGrade(89.1);
-            book.AddGrade(90.5);
-            book.AddGrade(77.5);
+
+            while (true)
+            {
+
+                Console.WriteLine("Enter a grade or 'q' to quit");
+                var input = Console.ReadLine();
+
+                if (input == "q")
+                {
+                    break;
+                }
+
+                try
+                {
+                    var grade = double.Parse(input);
+                    book.AddGrade(grade);
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+            }
+
             var stats = book.GetStatistics();
+
             Console.WriteLine($"The lowest grade is {stats.Low}");
             Console.WriteLine($"The highest grade is {stats.High}");
             Console.WriteLine($"The average grade is {stats.Average:N1}");
+            Console.WriteLine($"The letter grade is {stats.Letter}");
         }
     }
 
--- test/GradeBook.Tests/BookTests.cs
+++ test/GradeBook.Tests/BookTests.cs
@@ -8,20 +8,22 @@ namespace GradeBook.Tests
         [Fact]
         public void BookCalculatesAnAverageGrade()
         {
-            // arange
+            // arrange
             var book = new Book("");
             book.AddGrade(89.1);
             book.AddGrade(90.5);
             book.AddGrade(77.3);
 
             // act
-           var result = book.GetStatistics();
+            var result = book.GetStatistics();
 
             // assert
             Assert.Equal(85.6, result.Average, 1);
             Assert.Equal(90.5, result.High, 1);
             Assert.Equal(77.3, result.Low, 1);
+            Assert.Equal('B', result.Letter);
 
         }
+
     }
 }
